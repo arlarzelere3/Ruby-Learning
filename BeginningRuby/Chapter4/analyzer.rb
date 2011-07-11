c12c6ac966b3b6b7a3443f856bb9593d753c9412
Alex Larzelere <alarzelere@gmail.com>
2011-07-11 19:37:44 -0400

    - Added Chapter4

---
+++ BeginningRuby/Chapter4/analyzer.rb
@@ -0,0 +1,28 @@
+text = ''
+line_count = 0
+File.open("oliver.txt").each do |line|
+	line_count += 1
+	text << line
+end
+puts "#{line_count} lines"
+
+total_characters = text.length
+puts "#{total_characters} characters"
+
+total_characters_nospaces = 
+	text.gsub(/\s+/, '').length
+	puts "#{total_characters_nospaces} characters (excluding spaces)"
+
+word_count = text.split.length
+puts "#{word_count} words"
+
+paragraph_count = text.split(/\n\n/).length
+puts "#{paragraph_count} paragraphs"
+
+sentence_count = text.split(/\.|\?|!/).length
+puts "#{sentence_count} sentences"
+
+puts "#{sentence_count / paragraph_count} sentences per paragraph"
+
+puts "#{word_count / sentence_count} words per sentence"
+
