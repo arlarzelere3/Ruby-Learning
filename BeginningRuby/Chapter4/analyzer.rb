a7fc7f429635ac0bcd23478a8055572ae29fe2d2
Alex Larzelere <alarzelere@gmail.com>
2011-07-11 20:19:01 -0400

    - Updated analyzer.rb to final version

--- BeginningRuby/Chapter4/analyzer.rb
+++ BeginningRuby/Chapter4/analyzer.rb
@@ -1,28 +1,43 @@
-text = ''
-line_count = 0
-File.open("oliver.txt").each do |line|
-	line_count += 1
-	text << line
-end
-puts "#{line_count} lines"
+# analyzer.rb -- Text Analyzer
 
-total_characters = text.length
-puts "#{total_characters} characters"
+stopwords = %w{the a by on for of are with just but and to the my I has some in}
+lines = File.readlines(ARGV[0])
+line_count = lines.size
+text = lines.join
 
-total_characters_nospaces = 
-	text.gsub(/\s+/, '').length
-	puts "#{total_characters_nospaces} characters (excluding spaces)"
+# Count the characters
+character_count = text.length
+character_count_nospaces = text.gsub(/\s+/, '').length
 
+# Count the words, sentences, and paragraphs
 word_count = text.split.length
-puts "#{word_count} words"
-
-paragraph_count = text.split(/\n\n/).length
-puts "#{paragraph_count} paragraphs"
-
 sentence_count = text.split(/\.|\?|!/).length
+paragraph_count = text.split(/\n\n/).length
+
+# Make a list of words in the text that aren't stop words,
+# count them, and work out the percentage of non-stop words
+# against all words
+all_words = text.scan(/\w+/)
+good_words = all_words.select{ |word| !stopwords.include?(word) }
+good_percentage = ((good_words.length.to_f / all_words.length.to_f) * 100).to_i
+
+# Summarize the text by cherry picking some choice sentences
+sentences = text.gsub(/\s+/, ' ').strip.split(/\.|\?|!/)
+sentences_sorted = sentences.sort_by { |sentence| sentence.length }
+one_third = sentences_sorted.length / 3
+ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
+ideal_sentences = ideal_sentences.select { |sentence| sentence =~ /is|are/ }
+
+# Give the analysis back to the user
+puts "#{line_count} lines"
+puts "#{character_count} characters"
+puts "#{character_count_nospaces} characters (excluding spaces)"
+puts "#{word_count} words"
 puts "#{sentence_count} sentences"
-
-puts "#{sentence_count / paragraph_count} sentences per paragraph"
-
-puts "#{word_count / sentence_count} words per sentence"
+puts "#{paragraph_count} paragraphs"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{good_percentage}% of words are non-fluff words"
+puts "Summary:\n\n" + ideal_sentences.join(". ")
+puts "-- End of analysis"
 
